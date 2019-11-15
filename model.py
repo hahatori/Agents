a21b0d337ec2e6fa9b1f6f64510014b2c9ac705e
hahatori <56164812+hahatori@users.noreply.github.com>
2019-11-15 19:22:07 +0000

    Add files via upload
    I/O

--- model.py
+++ model.py
@@ -10,6 +10,8 @@ import random
 import operator
 import matplotlib.pyplot
 import agentframework
+import csv
+#import environment
 
 def distance_between(agents_row_a, agents_row_b):
     return (((agents_row_a.x - agents_row_b.x)**2) +
@@ -19,18 +21,48 @@ num_of_agents = 10
 num_of_iterations = 100
 agents = []
 
+
+
+#distance = [] 
+#f=open("in.txt",delimiter=',')
+#data = csv.reader(f)
+environment = []
+with open("in.txt") as f:
+    data = f.read().splitlines() 
+
+    for row in data:
+        rowlist = []
+        for value in row.split(','):
+            if value[-1] == '\\':
+                value1 = value[0:(len(value)-1)]
+                rowlist.append(int(value1))
+            else:
+                rowlist.append(int(value))
+        environment.append(rowlist)
+
+    
+
+
+
+for line in agents:
+    f.write(line)
+#f.close()
+
+
 # Make the agents.
 for i in range(num_of_agents):
-    agents.append(agentframework.Agent())
+    agents.append(agentframework.Agent(environment))
 
 # Move the agents.
 for j in range(num_of_iterations):
     for i in range(num_of_agents):
 
         agents[i].move()
-
+        agents[i].eat()
+        
 matplotlib.pyplot.xlim(0, 99)
 matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.imshow(environment)
 for i in range(num_of_agents):
     matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
 matplotlib.pyplot.show()
@@ -38,4 +70,15 @@ matplotlib.pyplot.show()
 for agents_row_a in agents:
     for agents_row_b in agents:
         distance = distance_between(agents_row_a, agents_row_b)
+ 
 
+
+      
+matplotlib.pyplot.imshow(environment)
+matplotlib.pyplot.show()
+
+
+        
+        
+        
+ 
