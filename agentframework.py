b9cba3f9d89952141a37b4db21916b97144a9b01
hahatori <56164812+hahatori@users.noreply.github.com>
2019-11-15 13:11:29 +0000

    Add files via upload

---
+++ agentframework.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 22:05:27 2019
+
+@author: toriliang
+"""
+import random 
+
+
+class Agent():
+    def __init__(self):
+        self.y = random.randint(0,99)
+        self.x = random.randint(0,99)
+    
+    def move(self):
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
+            
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100
