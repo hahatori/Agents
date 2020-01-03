fce7bde7a7d5bbf0fdeef0194e1ecb794c596ab6
hahatori <56164812+hahatori@users.noreply.github.com>
2020-01-03 18:30:16 +0000

    Update agentframework.py

--- agentframework.py
+++ agentframework.py
@@ -8,18 +8,24 @@ Created on Thu Nov  7 22:05:27 2019
 import random 
 
 
- class Agent():
-     def __init__(self):
-         self.y = random.randint(0,99)
-         self.x = random.randint(0,99)
+class Agent():
+    def __init__(self):
+        self.y = random.randint(0,99)
+        self.x = random.randint(0,99)
 
-     def move(self):
-         if random.random() < 0.5:
-             self.x = (self.x + 1) % 100
-         else:
-             self.x = (self.x - 1) % 100
+    def move(self):
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
 
-         if random.random() < 0.5:
-             self.y = (self.y + 1) % 100
-         else:
-             self.y = (self.y - 1) % 100 
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100 
+        
+a = Agent()
+print(a.y, a.x)
+
+a.move()
+print(a.y, a.x)
