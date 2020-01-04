d2c51655d77610870e7f3d6083db196e4304d779
hahatori <56164812+hahatori@users.noreply.github.com>
2020-01-04 15:44:09 +0000

    Update agentframework.py

--- agentframework.py
+++ agentframework.py
@@ -3,16 +3,20 @@
 """
 Created on Thu Nov  7 22:05:27 2019
 
-@author: toriliang
+@author: tori
 """
-import random 
 
+import random
 
+# Build the Agent class.
 class Agent():
+    
+    # Define the initialization method to assign values of x and y.
     def __init__(self):
         self.y = random.randint(0,99)
         self.x = random.randint(0,99)
-
+        
+    # Define the "move" method to change x and y randomly. 
     def move(self):
         if random.random() < 0.5:
             self.x = (self.x + 1) % 100
@@ -23,7 +27,9 @@ class Agent():
             self.y = (self.y + 1) % 100
         else:
             self.y = (self.y - 1) % 100 
-        
+
+            
+# creat objects, test the output random numbers y and x.             
 a = Agent()
 print(a.y, a.x)
 
